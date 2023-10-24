150f234b7254a8b3e439ad6973b87b2c5f6e2f63
Omkar <omku95@gmail.com>
2023-10-24 12:52:24 -0400

    removed git makefile and changed tests. Now run tests via Makefile

--- tests/test_notion_api.py
+++ tests/test_notion_api.py
@@ -1,55 +1,41 @@
 import unittest
-from unittest.mock import patch, Mock
-from src.summarizer.summarize import Summarizer
+from unittest.mock import patch
+from src.notion import api
 from src.utils.logger import get_logger
 
-class TestSummarizer(unittest.TestCase):
+class TestNotionAPI(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.logger = get_logger(__name__)
-        cls.logger.info("Starting tests for Summarizer")
+        cls.logger.info("Starting tests for Notion API")
 
-    @patch('src.summarizer.summarize.pipeline')
-    def test_summarizer_initialization(self, mock_pipeline):
-        self.logger.info("Running test_summarizer_initialization")
-        mock_pipeline.return_value = Mock()
-        try:
-            summarizer = Summarizer()
-            self.logger.info("Summarizer initialization test passed.")
-        except Exception as e:
-            self.logger.error(f"Summarizer initialization test failed. Error: {e}")
-            self.fail("Failed to initialize the Summarizer.")
+    @patch('src.notion.api.requests.post')
+    def test_search_notion_pages(self, mock_post):
+        self.logger.info("Running test_search_notion_pages")
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = {'object': 'list', 'results': []}
+        mock_post.return_value = mock_response
+        result = api.search_notion_pages()
+        self.assertEqual(result, {'object': 'list', 'results': []})
 
-    @patch('src.summarizer.summarize.pipeline')
-    def test_summarize(self, mock_pipeline):
-        self.logger.info("Running test_summarize")
-        mock_model = Mock()
-        mock_model.return_value = [{'summary_text': 'This is a summary.'}]
-        mock_pipeline.return_value = mock_model
+    @patch('src.notion.api.requests.get')
+    def test_fetch_page_content(self, mock_get):
+        self.logger.info("Running test_fetch_page_content")
+        mock_response = unittest.mock.Mock()
+        mock_response.json.return_value = {'results': []}
+        mock_get.return_value = mock_response
 
-        text = "This is a long text that needs to be summarized."
-        summary = self.summarizer.summarize(text)
-        
-        self.assertEqual(summary, 'This is a summary.')
-        self.logger.info("Summarize test passed.")
-
-    @patch('src.summarizer.summarize.pipeline')
-    def test_summarize_parallel(self, mock_pipeline):
-        self.logger.info("Running test_summarize_parallel")
-        mock_model = Mock()
-        mock_model.return_value = [{'summary_text': 'This is a summary.'}]
-        mock_pipeline.return_value = mock_model
-
-        texts = ["This is a long text that needs to be summarized.", "Another text to summarize."]
-        summaries = self.summarizer.summarize_parallel(texts)
-        
-        self.assertEqual(summaries, ['This is a summary.', 'This is a summary.'])
-        self.logger.info("Summarize parallel test passed.")
+        page_id = "sample_page_id"
+        result = api.fetch_page_content(page_id)
+        self.assertEqual(result, {'results': []})
 
     @classmethod
     def tearDownClass(cls):
-        cls.logger.info("Finished tests for Summarizer")
+        cls.logger.info("Finished tests for Notion API")
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
